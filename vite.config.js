7cbc23ce64be6f27ac6980b3d63f6bfb131eed41
Prashi <prashivyas05@gmail.com>
2025-12-16 15:35:19 +0530

    updated script

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+ 
 
+
+  {/* export default defineConfig({
+    plugins: [
+      tailwindcss(),
+    ],
+  }) */}
+  
+  
+  export default defineConfig({
+    base: '/-Uifry-/', // <-- Replace with your repo name
+    plugins: [react()],
+  })
 
 
 
